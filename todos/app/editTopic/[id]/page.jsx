cac9835c2f27504fb6ba8a2702d18b9f6dc6e089
Kalpesh Lambade <123794408+KalpeshLambade@users.noreply.github.com>
2023-08-26 08:49:08 +0530

    updated

--- todos/app/editTopic/[id]/page.jsx
+++ todos/app/editTopic/[id]/page.jsx
@@ -1,10 +1,31 @@
 import EditTopicForm from "@/components/EditTopicForm";
+import axios from "axios";
 import React from "react";
 
-const editTopic = () => {
-  return <>
-    <EditTopicForm/>
-  </>;
+const getTopicById = async (id) => {
+  try {
+    const res = await axios.get(`http://localhost:3000/api/topics/${id}`);
+
+    if (res.status === 200) {
+      return res.data.topic;
+    } else {
+      alert("Data not found");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const editTopic = async ({ params }) => {
+  const { id } = params;
+  const topics = await getTopicById(id);
+  const { title, description } = topics;
+
+  return (
+    <>
+      <EditTopicForm id={id} title={title} description={description}/>
+    </>
+  );
 };
 
 export default editTopic;
--- todos/components/EditTopicForm.jsx
+++ todos/components/EditTopicForm.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+"use client";
+import axios from "axios";
+import React, { useState } from "react";
 
-const EditTopicForm = () => {
+const EditTopicForm = ({ id ,title,description}) => {
+  const [newTopic, setNewTopic] = useState({ title: title, description: description });
+
+  console.log(newTopic);
+
+  const handelChange = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    setNewTopic({ ...newTopic, [name]: value });
+  };
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    
+  };
+
   return (
     <>
       <form className="flex flex-col gap-3">
@@ -8,13 +25,20 @@ const EditTopicForm = () => {
           type="text"
           placeholder="Topic Title"
           className="border border-slate-500 px-8 py-2"
+          onChange={handelChange}
+          name="title"
         />
         <input
           type="text"
           placeholder="Topic Description"
           className="border border-slate-500 px-8 py-2"
+          onChange={handelChange}
+          name="description"
         />
-        <button className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
+        <button
+          className="bg-green-600 font-bold text-white py-3 px-6 w-fit"
+          onClick={handelSubmit}
+        >
           Update Topic
         </button>
       </form>
